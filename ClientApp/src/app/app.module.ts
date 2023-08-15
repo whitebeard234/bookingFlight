03e17caf863e0dda99de1effcca3c39c280a9e69
Marc Coritico <mcoritico@officebrands.com.au>
2023-08-15 15:14:49 +0800

    First commit

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
+import { BookFlightComponent } from './book-flight/book-flight.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    SearchFlightsComponent,
+    BookFlightComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: SearchFlightsComponent, pathMatch: 'full' },
+      { path: 'search-flights', component: SearchFlightsComponent },
+      { path: 'book-flight/:flightId', component: BookFlightComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
